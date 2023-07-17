b9bd6630fefba207fdbf8863ca20b8b429f130a2
CodeName29 <mohamedhedi322@gmail.com>
2023-07-17 11:12:46 +0100

    Changed the HTML for the site.

--- beachapp/blog/views.py
+++ beachapp/blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def log_in(request):
+    return render(request, 'blog/log_in.html', {})
+
+def sign_up(request):
+    return render(request, 'blog/sign_up.html', {})
+
+
