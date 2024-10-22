87761346b3b860c2d999bb81aed0ae1b9067fc60
CodeName29 <mohamedhedi322@gmail.com>
2024-10-22 18:08:31 +0100

    initial commit

---
+++ beachapp/blog/static/js/verif.js
@@ -0,0 +1,5 @@
+Nm=F.N.value;
+if((Nm.length>=20)||(Nm.length<=2)){
+    alert("Username Invalide!!");
+    return false;
+}
--- beachapp/blog/views.py
+++ beachapp/blog/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-
+from django.utils import timezone
+from .models import Post
 # Create your views here.
 def log_in(request):
-    return render(request, 'blog/log_in.html', {})
+    posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/log_in.html', {'posts': posts})
 
 def sign_up(request):
     return render(request, 'blog/sign_up.html', {})
+
+
 
 
